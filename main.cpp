62cd57b6cb03879369c034b2908f5616925d0521
Ben K. Sladerㅤㅤㅤㅤ <klockcraft-coders-official@outlook.com>
2024-06-07 13:31:08 -0400

    Update #0 - release a demo

--- main.cpp
+++ main.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
+#include <opencv2/highgui/highgui.hpp>
+using namespace cv;
 using namespace std;
 
 int main() {
     cout << "Coming soon!" << endl;
+    
+    Mat myImage; 
+    namedWindow("PhotoFrame"); 
+    myImage = imread("promotional.jpg"); 
+    if (myImage.empty()) { 
+        cout << "Couldn't load the image." << endl;
+        system("pause"); 
+        return -1;
+    }
+    imshow("PhotoFrame", myImage); 
+    waitKey(0); 
     return 0;
 }
